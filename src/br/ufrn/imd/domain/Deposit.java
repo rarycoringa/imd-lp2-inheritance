8cd8bffb302de79ec7c4389d02e55cc05faf5535
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2022-09-20 14:46:34 -0300

    add deposit object without required methods yet

---
+++ src/br/ufrn/imd/domain/Deposit.java
@@ -0,0 +1,24 @@
+package br.ufrn.imd.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufrn.imd.domain.Product;
+
+public class Deposit {
+	private List<Product> products;
+	
+	public Deposit() {
+		this.products = new ArrayList<Product>();
+	}
+	
+	// add products
+	
+	// remove products
+	
+	// get products length
+	
+	// verify if deposit is empty
+	
+	// get more expensive product
+}
