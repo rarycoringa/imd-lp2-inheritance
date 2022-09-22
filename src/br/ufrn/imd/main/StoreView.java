21ffd1350c13b430e3c3a31c4db006b68eec8ef9
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2022-09-22 14:25:21 -0300

    create StoreView class in main package

---
+++ src/br/ufrn/imd/main/StoreView.java
@@ -0,0 +1,38 @@
+package br.ufrn.imd.main;
+
+import java.util.Calendar;
+
+import br.ufrn.imd.domain.DurableProduct;
+
+public class StoreView {
+	public static void main(String[] args) {
+		Calendar productionDate = Calendar.getInstance();
+		productionDate.set(Calendar.DATE, 22);
+		productionDate.set(Calendar.MONTH, 9);
+		productionDate.set(Calendar.YEAR, 2022);
+		
+		DurableProduct car = new DurableProduct();
+		car.setName("M3");
+		car.setPrice(69900.00);
+		car.setBrand("BMW");
+		car.setDescription("An amazing car");
+		car.setProductionDate(productionDate.getTime());
+		car.setDurability(10);
+
+		DurableProduct book = new DurableProduct();
+		book.setName("Harry Potter");
+		book.setPrice(34.90);
+		book.setBrand("J. K. Rowling");
+		book.setDescription("A magic book");
+		book.setProductionDate(productionDate.getTime());
+		book.setDurability(20);
+		
+		DurableProduct phone = new DurableProduct();
+		phone.setName("iPhone 13");
+		phone.setPrice(999.90);
+		phone.setBrand("Apple");
+		phone.setDescription("A futuristic phone");
+		phone.setProductionDate(productionDate.getTime());
+		phone.setDurability(5);
+	}
+}
